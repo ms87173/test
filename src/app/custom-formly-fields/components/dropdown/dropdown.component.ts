13d8e41c0538155e86f3d071cc809bbfd76cef54
Karan Sharma 4 <karsharm7@publicisgroupe.net>
2018-08-24 12:48:00 +0530

    dropdown component formly

---
+++ src/app/custom-formly-fields/components/dropdown/dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FieldType } from "@ngx-formly/core";
+
+@Component({
+  selector: 'dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DropdownComponent extends FieldType {
+
+
+}
--- src/app/custom-formly-fields/custom-formly-fields.module.ts
+++ src/app/custom-formly-fields/custom-formly-fields.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [DropdownComponent]
 })
 export class CustomFormlyFieldsModule { }
