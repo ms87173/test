e940369d4ae3e5d2553eb7b6f71f9c139c09f314
Karan Sharma 4 <karsharm7@publicisgroupe.net>
2018-08-27 09:55:05 +0530

    dropdown config edit

--- src/app/storybook/formly-consumer/formly-consumer.component.ts
+++ src/app/storybook/formly-consumer/formly-consumer.component.ts
@@ -21,62 +21,16 @@ export class FormlyConsumerComponent implements OnInit {
       templateOptions: {
         label: 'Normal Select',
         options: [
-          {label: 'Iron Man', value: 'iron_man'},
-          {label: 'Captain America', value: 'captain_america'},
-          {label: 'Black Widow', value: 'black_widow'},
-          {label: 'Hulk', value: 'hulk'},
-          {label: 'Captain Marvel', value: 'captain_marvel'},
+          { label: 'Iron Man', value: 'iron_man' },
+          { label: 'Captain America', value: 'captain_america' },
+          { label: 'Black Widow', value: 'black_widow' },
+          { label: 'Hulk', value: 'hulk' },
+          { label: 'Captain Marvel', value: 'captain_marvel' },
         ],
-      },
-    },
-    {
-      key: 'marvel2',
-      type: 'select',
-      templateOptions: {
-        label: 'Grouped Select',
-        options: [
-          {label: 'Iron Man', value: 'iron_man', group: 'Male'},
-          {label: 'Captain America', value: 'captain_america', group: 'Male'},
-          {label: 'Black Widow', value: 'black_widow', group: 'Female'},
-          {label: 'Hulk', value: 'hulk', group: 'Male'},
-          {label: 'Captain Marvel', value: 'captain_marvel', group: 'Female'},
-        ],
-      },
-    },
-    {
-      key: 'marvel3',
-      type: 'select',
-      templateOptions: {
-        label: 'Select with custom name/value/group',
-        options: [
-          {label: 'Iron Man', id: 'iron_man', gender: 'Male'},
-          {label: 'Captain America', id: 'captain_america', gender: 'Male'},
-          {label: 'Black Widow', id: 'black_widow', gender: 'Female'},
-          {label: 'Hulk', id: 'hulk', gender: 'Male'},
-          {label: 'Captain Marvel', id: 'captain_marvel', gender: 'Female'},
-        ],
-        groupProp: 'gender',
-        valueProp: 'id',
-        labelProp: 'label',
-      },
-    },
-    {
-      key: 'multiselect',
-      type: 'select',
-      templateOptions: {
-        label: 'Multi-select',
-        multiple: true,
-        options: [
-          {label: 'Iron Man', value: 'iron_man'},
-          {label: 'Captain America', value: 'captain_america'},
-          {label: 'Black Widow', value: 'black_widow'},
-          {label: 'Hulk', value: 'hulk'},
-          {label: 'Captain Marvel', value: 'captain_marvel'},
-        ],
-      },
+      }
     },
   ];
-  constructor() {}
+  constructor() { }
   ngOnInit() {
   }
 
