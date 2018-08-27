61a27b39e02f9315d2a7a5f265f9df9d6610d90f
karan2227 <karansharma768@gmail.com>
2018-08-27 13:01:26 +0530

    selectors | reducer change. | development query

--- src/app/store/selectors/application-request.selectors.ts
+++ src/app/store/selectors/application-request.selectors.ts
@@ -1,15 +1,31 @@
 import * as fromStore from '../reducers/';
-import * as fromApplicationRequest from '../reducers/application-request.reducers';
+import * as fromApplicationRequest from '../reducers/application-request.reducer';
 import { createSelector } from '@ngrx/store';
 
 export const getApplicaitonRequestState = (state: fromStore.AppState) => state.applicationRequest;
 
-export const getApplicaitonRequest = createSelector(
+export const getApplicaitonState = createSelector(
     getApplicaitonRequestState,
-    fromApplicationRequest.getApplicaitonRequets
+    fromApplicationRequest.getApplicaitonRequestState
 );
 
-export const getApplicationRequestWorkflows = createSelector(
+export const getApplicationWorkflowsState = createSelector(
+    getApplicaitonRequestState,
+    fromApplicationRequest.getApplicationWorkflowsState
+);
+
+export const getApplicaiton = createSelector(
+    getApplicaitonRequestState,
+    fromApplicationRequest.getApplication
+);
+
+export const getApplicationWorkflows = createSelector(
     getApplicaitonRequestState,
     fromApplicationRequest.getApplicationWorkflows
 );
+
+export const getApplicationActiveTask = createSelector(
+    getApplicaitonRequestState,
+    fromApplicationRequest.getApplicationActiveTask
+);
+
