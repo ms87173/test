f8d5e3dee3c413a2fd21c39e32ba194bf7b6122a
karan2227 <karansharma768@gmail.com>
2018-08-30 01:11:47 +0530

    removal of unnecessary code from project

--- src/app/test-feature/models/product.ts
+++
@@ -1,5 +0,0 @@
-export class Product {
-    id:  number;
-    name : string ;
-    description: string;
-}
--- src/app/test-feature/test-feature-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductsListComponent } from './products-list/products-list.component';
-
-export const TestFeatureRoutes: Routes = [
-    {
-        path: '',
-        component: ProductsListComponent
-    }
-]
