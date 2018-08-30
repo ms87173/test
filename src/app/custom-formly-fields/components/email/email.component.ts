0fe25fb0af609997addc13e49dd74a4a454dcbff
Tinshu Sasi <tinsasi@publicisgroupe.net>
2018-08-30 12:26:47 +0530

    commiting temporary changes for Email component

--- src/app/custom-formly-fields/components/email/email.component.ts
+++ src/app/custom-formly-fields/components/email/email.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewEncapsulation } from '@angular/core';
 import { FormlyFormBuilder, FieldArrayType } from '@ngx-formly/core';
 
 @Component({
   selector: 'email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.css']
+  styleUrls: ['./email.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EmailComponent extends FieldArrayType implements OnInit, OnChanges {
   questionType: string = "custom-email";
