4fde348999336c83cb73a8bad3921afb15159341
Tinshu Sasi <tinsasi@publicisgroupe.net>
2018-09-04 15:27:38 +0530

    Commiting changes to resolve conflict

--- src/app/core/services/dynamic-options.service.ts
+++ src/app/core/services/dynamic-options.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 
-@Injectable() export class DynamicOptionsService {
+@Injectable()
+ export class DynamicOptionsService {
   constructor(private apiService: ApiService) { }
   getDynamicOptions(param: string): Observable<any[]> {
     //TODO: need to implement against a param value
