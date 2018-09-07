39d6205b999eafd787c04a9e62eac506ca53faab
Tinshu Sasi <tinsasi@publicisgroupe.net>
2018-09-07 16:18:14 +0530

    changes  for filtering unwanted components

--- src/app/custom-formly-fields/enums/custom-components.enum.ts
+++ src/app/custom-formly-fields/enums/custom-components.enum.ts
@@ -10,3 +10,16 @@ export enum CustomComponentsEnum {
     CUSTOM_EMAIL = 'custom-email',
     CUSTOM_PHONE = 'custom-phone',   
 }
+
+export const EXISTING_COMPONENTS:string[]=[
+    CustomComponentsEnum.CUSTOM_CHECKBOX,
+    CustomComponentsEnum.CUSTOM_DROPDOWN,
+    CustomComponentsEnum.CUSTOM_RADIO_BUTTON,
+    // CustomComponentsEnum.CUSTOM_TYPEAHEAD,
+    CustomComponentsEnum.CUSTOM_DATEPICKER,
+    // CustomComponentsEnum.CUSTOM_ADDRESS_FIELDS,
+    // CustomComponentsEnum.CUSTOM_ADDRESS_PROOF,
+    CustomComponentsEnum.CUSTOM_TEXTBOX,
+    // CustomComponentsEnum.CUSTOM_EMAIL,
+    // CustomComponentsEnum.CUSTOM_PHONE
+];
