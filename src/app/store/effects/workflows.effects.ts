090e6a0fd3cdf47ea296e350895282563eda75d6
Vikash Kumar 8 <vikkumar12@publicisgroupe.net>
2018-09-20 15:47:49 +0530

    refresh side navigation code

--- src/app/store/effects/workflows.effects.ts
+++ src/app/store/effects/workflows.effects.ts
@@ -4,16 +4,15 @@ import { Effect, Actions, ofType } from '@ngrx/effects';
 import {
     ActionTypes,
     GetApplicationWorkflows,
-    GetApplicationWorkflowsFailure,
-    GetApplicationWorkflowsSuccess,
     SetActiveTask,
     SaveActiveTaskAndNext,
     SaveActiveTaskAndExit,
 } from '../actions/workflows.action';
-import { switchMap, map, catchError, filter } from 'rxjs/operators';
+import { switchMap, map, catchError, filter, mergeMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import * as fromRouterActions from '../actions/router.actions';
 import { RouterGo } from '../actions/router.actions';
+import { GetApplicationRequest } from '../actions/application.actions';
 
 @Injectable()
 export class ApplicationWorkflowsEffects {
@@ -26,84 +25,46 @@ export class ApplicationWorkflowsEffects {
             return new GetApplicationWorkflows(applicationId);
         })
     );
-    // @Effect() getApplicationRequestWorkflowsEffect = this.actions$.pipe(
-    //     ofType(ActionTypes.GET_APPLICATION_REQUEST_WORKFLOWS),
-    //     switchMap(
-    //         (action: GetApplicationWorkflows) =>
-    //             this.applicationRequestService.getApplicationRequestWorkflows(action.payload)
-    //                 .pipe(
-    //                     map(
-    //                         (workflows) => {
-    //                             let firstTaskId = '', lastTaskId = '';
-    //                             if (workflows && workflows.length > 0) {
-    //                                 const lastWorkflowIndex = workflows.length - 1;
-    //                                 const lastWorkflow = workflows[lastWorkflowIndex];
-    //                                 const tlen = lastWorkflow && lastWorkflow.tasks && lastWorkflow.tasks.length;
-    //                                 const lastTask = lastWorkflow && lastWorkflow.tasks && lastWorkflow.tasks[tlen - 1];
-    //                                 lastTaskId = lastTask && lastTask.id;
-    //                                 firstTaskId = workflows[0] &&
-    //                                     workflows[0].tasks &&
-    //                                     workflows[0].tasks[0] &&
-    //                                     workflows[0].tasks[0].id;
-    //                             }
-    //                             return new GetApplicationWorkflowsSuccess({
-    //                                 workflows,
-    //                                 lastTaskId,
-    //                                 firstTaskId
-    //                             });
-    //                         }
-    //                     ),
-    //                     catchError(
-    //                         (err) => of(new GetApplicationWorkflowsFailure(err))
-    //                     )
-    //                 )
-    //     ));
-        @Effect() saveAndNextRequestTaskEffect = this.actions$.pipe(
-            ofType(ActionTypes.SAVE_NEXT_REQUEST_ACTIVE_TASK),
-            switchMap((action: SaveActiveTaskAndNext) =>
-                this.applicationRequestService
-                    .saveApplicationRequestTask(action.payload.current)
-                    .pipe(
-                        map(response => {
-                            const { workflowId, taskId} = action.payload;
-                            //console.log(response);
-                            //console.log(`Task is Saved Redirecting to next task with workflowId ${workflowId} and taskId ${taskId}`);
-                            return new SetActiveTask({
+    @Effect() saveAndNextRequestTaskEffect = this.actions$.pipe(
+        ofType(ActionTypes.SAVE_NEXT_REQUEST_ACTIVE_TASK),
+        switchMap((action: SaveActiveTaskAndNext) =>
+            this.applicationRequestService
+                .saveApplicationRequestTask(action.payload.current)
+                .pipe(
+                    mergeMap((response) => {
+                        const { workflowId, taskId } = action.payload;
+                        const { requestId } = action.payload.current;
+                        return [
+                            new GetApplicationRequest(requestId),
+                            new SetActiveTask({
                                 workflowId,
                                 taskId
-                            });
-                        }),
-                        catchError(error => {
-                            //console.log(error);
-                            return of({}
-                            // new RouterGo({
-                            //     path: ['ddo', 'error', { ...error }]
-                            // }))
-                        );
+                            })
+                        ];
+                    }),
+                    catchError(error => {
+                        console.log(error);
+                        return of({});
                     })
-                    )
-                ));
-        @Effect() saveAndExitRequestTaskEffect = this.actions$.pipe(
-            ofType(ActionTypes.SAVE_EXIT_REQUEST_ACTIVE_TASK),
-            switchMap((action: SaveActiveTaskAndExit) =>
-                this.applicationRequestService
-                    .saveApplicationRequestTask(action.payload)
-                    .pipe(
-                        map(response => {
-                            //console.log(response);
-                            //console.log('Task is Saved');
-                            return new RouterGo({
-                                path: ['ddo', 'my-applications']
-                            });
-                        }),
-                        catchError(
-                            error => of(
-                                new RouterGo({
-                                    path: ['ddo', 'error', { ...error }]
-                                }))
-                        )
-                    )
-                ));
+                )
+        ));
+    @Effect() saveAndExitRequestTaskEffect = this.actions$.pipe(
+        ofType(ActionTypes.SAVE_EXIT_REQUEST_ACTIVE_TASK),
+        switchMap((action: SaveActiveTaskAndExit) =>
+            this.applicationRequestService
+                .saveApplicationRequestTask(action.payload)
+                .pipe(
+                    map(response => {
+                        return new RouterGo({
+                            path: ['ddo', 'my-applications']
+                        });
+                    }),
+                    catchError(error => {
+                        console.log(error);
+                        return of({});
+                    })
+                )
+        ));
 
     constructor(
         private applicationRequestService: ApplicationRequestService,
